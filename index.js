e5d051988db904eac77f485e953bf9574c4b9dcc
Jordan Pitlor <jordan@pitlor.dev>
2020-07-27 20:11:34 -0400

    fix the code

--- index.js
+++ index.js
@@ -12,23 +12,39 @@ const connection = new ewelink({
 let fan;
 
 const Channel = {
-    FAN_LOW: 1,
-    FAN_MEDIUM: 2,
-    FAN_HIGH: 3,
-    LIGHT: 4,
+    FAN_LOW: 2,
+    FAN_MEDIUM: 3,
+    FAN_HIGH: 4,
+    LIGHT: 1,
 }
-const RFID_PIN = 1;
+const ENTER_ROOM_PIN = 10;
+const EXIT_ROOM_PIN = 12;
 
-(async () => {
-    const [response] = await connection.getDevices();
-    fan = response.deviceid;
+connection
+    .getDevices()
+    .then(([response]) => {
+        fan = response.deviceid;
+        logger.info(`Fan ID: ${fan}`);
+    });
 
-    pi.setup(RFID_PIN, 'in', 'rising', (error) => {
-        if (error) {
-            logger.error(error);
-            return;
-        }
+function logError(e) {
+    if (e) {
+        logger.error(e);
+    }
+}
 
-        await connection.setDevicePowerState(fan, Channel.LIGHT, 'toggle');
-    })
-})();
+pi.on('change', async (channel) => {
+    logger.info(`Detected a motion in channel ${channel}!`);
+    logger.info(`Person just ${channel === ENTER_ROOM_PIN ? 'entered' : 'exited'} the room`);
+
+    if (!fan) {
+        logger.error('API not ready yet!');
+        return;
+    }
+
+    await connection.setDevicePowerState(fan, channel === ENTER_ROOM_PIN ? 'on' : 'off', Channel.LIGHT);
+});
+
+pi.setup(ENTER_ROOM_PIN, pi.DIR_IN, pi.EDGE_RISING, logError);
+pi.setup(EXIT_ROOM_PIN, pi.DIR_IN, pi.EDGE_RISING, logError);
+
